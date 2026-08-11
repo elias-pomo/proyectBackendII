ee21cc8bd7fce231d8bef2ef31efea189036dfdd
Elias Pomo <eliaspomo2024@gmail.com>
2026-08-10 22:45:46 -0300

    enlace1

---
+++ src/routes/tickets.routes.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    
+})
+
+router.get('/:id', (req, res) => {
+
+})
+
+router.post('/', (req, res) => {
+
+})
+
+export default router;
